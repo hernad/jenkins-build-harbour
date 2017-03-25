cfb85198eef278fda7a35b64abcd614ee48bcc72
hernad <hernad@bring.out.ba>
2017-03-25 10:27:20 +0100

    build_harbour contains all of the build logic

--- build_harbour.sh
+++ build_harbour.sh
@@ -6,11 +6,10 @@
 [ -f harbour.tar.gz ] && rm harbour.tar.gz
 
 vagrant --version
-vagrant up
-vagrant provision
+vagrant up --provision
 vagrant halt
 
-if [ ! -f LO.tar.gz ] ; then
+if [ ! -f harbour.tar.gz ] ; then
    echo "harbour.tar.gz not created ?!"
    exit 1
 fi
