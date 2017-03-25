165b1800d06208810557cfe149271b58e0f4ba8f
hernad <hernad@bring.out.ba>
2017-03-25 10:23:39 +0100

    build_harbour contains all of the build logic

--- build_harbour.sh
+++ build_harbour.sh
@@ -2,3 +2,15 @@
 
 [ -f data.vmdk ] || VBoxManage clonehd ~/data_core_base_windows.vmdk data.vmdk
 
+
+[ -f harbour.tar.gz ] && rm harbour.tar.gz
+
+vagrant --version
+vagrant up
+vagrant provision
+vagrant halt
+
+if [ ! -f LO.tar.gz ] ; then
+   echo "harbour.tar.gz not created ?!"
+   exit 1
+fi
