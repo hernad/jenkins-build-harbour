1f232ba27d4cd6ffe2fbc1b7c9ae96dfcc4ce269
hernad <hernad@bring.out.ba>
2017-03-25 17:49:14 +0100

    .ssh_download_key

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -19,7 +19,10 @@ SSH_DOWNLOAD_SERVER=docker@192.168.168.171
 
 DOWNLOADS_DIR=/data_0/f18-downloads_0/downloads.bring.out.ba/www/files/
 
+echo "ssh_download_key:"
 cat .ssh_download_key
+echo "===================="
+
 echo "scp $SSH_DOWNLOAD_SERVER $DOWNLOADS_DIR" 
 scp -i .ssh_download_key \
   -o StrictHostKeyChecking=no \
