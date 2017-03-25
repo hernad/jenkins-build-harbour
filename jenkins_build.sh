7a49448bb6fb77787c4dde0bcb6d47b73427d296
hernad <hernad@bring.out.ba>
2017-03-25 13:11:30 +0100

    .ssh_download_key

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -17,6 +17,9 @@ fi
 
 SSH_DOWNLOAD_SERVER=docker@192.168.168.171
 
-scp -i .ssh_download_key  hbwin.tar.gz $SSH_DOWNLOAD_SERVER:/data_0/f18-downloads_0/downloads.bring.out.ba/www/files/
+scp -i .ssh_download_key \
+ -o StrictHostKeyChecking=no \
+ hbwin.tar.gz \
+ $SSH_DOWNLOAD_SERVER:/data_0/f18-downloads_0/downloads.bring.out.ba/www/files/
 
 
