31aed124675b7da75e8438d7a5e4c4f1c0483618
hernad <hernad@bring.out.ba>
2017-03-25 11:27:34 +0100

    //vboxsrv/vagrant

--- build_harbour_msys2.sh
+++ build_harbour_msys2.sh
@@ -2,4 +2,4 @@
 
 echo connecting to msys2 ssh server and running commands
 
-ssh -p 2222 vagrant@127.0.0.1 //vagrant/msys2_build_step_3.sh
+ssh -p 2222 vagrant@127.0.0.1 //vboxsrv/vagrant/msys2_build_step_3.sh
--- vagrant.cmd
+++ vagrant.cmd
@@ -2,10 +2,5 @@ cscript slmgr.vbs /ato
 
 REM C:\cygwin\bin\bash -l "%~f0" "%CD%" %_args%
 
-#C:\cygwin\bin\bash -l -c "//vboxsrv/vagrant/build_harbour.sh  --init" 
+C:\cygwin\bin\bash -l -c "//vboxsrv/vagrant/build_harbour_msys2.sh"
 
-cd \
-dir c:
-
-f:
-dir \
