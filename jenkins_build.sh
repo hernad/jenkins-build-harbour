055860ef2a5feb3b70cef3cb40531cdd89a58cce
hernad <hernad@bring.out.ba>
2017-03-26 14:24:54 +0200

    hbwin.tar.gz

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 SSH_DOWNLOAD_SERVER=docker@192.168.168.171
-FILE=hbwin.tar.gz
+FILE="hbwin.tar.gz"
 DOWNLOADS_DIR=/data_0/f18-downloads_0/downloads.bring.out.ba/www/files/
 
 [ -f data.vmdk ] || VBoxManage clonehd ~/data_core_base_windows.vmdk data.vmdk
 
-[ -f $FILE.tar.gz ] && rm $FILE.tar.gz
+[ -f $FILE ] && rm $FILE
 
 vagrant --version
 vagrant up --provision
 vagrant halt
 
-if [ ! -f $FILEh ] ; then
+if [ ! -f $FILE ] ; then
    echo "$FILE not created ?!"
    exit 1
 fi
@@ -23,7 +23,7 @@ echo "scp $SSH_DOWNLOAD_SERVER $DOWNLOADS_DIR"
 
 scp -i .ssh_download_key \
   -o StrictHostKeyChecking=no \
-  hbwin.tar.gz \
+ $FILE \
  $SSH_DOWNLOAD_SERVER:$DOWNLOADS_DIR
 
 ssh -i .ssh_download_key  $SSH_DOWNLOAD_SERVER  ls -lh $DOWNLOADS_DIR/$FILE
