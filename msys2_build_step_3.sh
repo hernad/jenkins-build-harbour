47f1913184589404a62210ecfda750b17691fb57
hernad <hernad@bring.out.ba>
2017-03-26 14:10:20 +0200

    HB_WITH_PGSQL

--- msys2_build_step_3.sh
+++ msys2_build_step_3.sh
@@ -6,10 +6,12 @@ echo $PATH
 export PATH=/c/msys32/mingw32/bin:C:\hbwin\bin:/mingw32/bin:/usr/local/bin:/usr/bin:/bin
 export PATH=$PATH:/c/ProgramData/Oracle/Java/javapath
 export PATH=$PATH:/c/WINDOWS/system32:/c/WINDOWS:/c/WINDOWS/System32/Wbem:/c/WINDOWS/System32/WindowsPowerShell/v1.0
-#export PATH=$PATH:/c/Program Files (x86)/Windows Kits/10/Windows Performance Toolkit:/c/Program Files (x86)/nodejs
-#export PATH=$PATH:/c/Program Files (x86)/Java/jdk1.8.0_72/bin
 
-#/c/Program Files (x86)/Microsoft SDKs/TypeScript/1.0:%SystemRoot%/system32:%SystemRoot%:%SystemRoot%/System32/Wbem:%SYSTEMROOT%/System32/WindowsPowerShell/v1.0:/c/Users/Ernad/AppData/Roaming/npm:/c/Users/Ernad/AppData/Local/atom/bin:/c/Program Files (x86)/Microsoft VS Code/bin:/c/hbwin/bin:/c/tools/apache-maven-3.3.3/bin:/c/knowhowERP/util:/c/knowhowERP/lib:%USERPROFILE%/AppData/Local/Microsoft/WindowsApps:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
+export ACLOCAL_PATH=/mingw32/share/aclocal:/usr/share/aclocal
+
+export MANPATH=/mingw32/share/man:/usr/local/man:/usr/share/man:/usr/man:/share/man:
+export MINGW_MOUNT_POINT=/mingw32
+export MSYS2='c:\msys32\mingw32'
 
 echo msys2 build step 3
 
@@ -25,6 +27,8 @@ git checkout my-master -f
 git pull
 
 
+export HB_WITH_OPENSSL='c:\msys32\mingw32\include'
+export HB_WITH_PGSQL='c:\msys32\mingw32\include'
 export HB_ARCHITECTURE=win
 export HB_COMPILER=mingw
 
