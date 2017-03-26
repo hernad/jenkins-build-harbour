56720a8281bd4c1e23614ba83cdb89552b97aa3b
hernad <hernad@bring.out.ba>
2017-03-26 14:12:17 +0200

    build run_msys2.sh

--- vagrant.cmd
+++ vagrant.cmd
@@ -2,5 +2,5 @@ cscript slmgr.vbs /ato
 
 REM C:\cygwin\bin\bash -l "%~f0" "%CD%" %_args%
 
-C:\cygwin\bin\bash -l -c "//vboxsrv/vagrant/build_harbour_msys2.sh"
+C:\cygwin\bin\bash -l -c "//vboxsrv/vagrant/run_msys2.sh"
 
