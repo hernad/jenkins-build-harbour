339d344b6b3c6534001fec17a452b96e48eaca39
hernad <hernad@bring.out.ba>
2017-05-03 10:04:41 +0200

    vdi

--- prepare_build.sh
+++ prepare_build.sh
@@ -3,5 +3,5 @@
 VDI_BASE=~/data_harbour_core_linux.vdi 
 [ ! -f $VDI_BASE ]  || VBoxManage createmedium --size 20000 --format VDI --filename $VDI_BASE
 
-[ -f data.vmdk ] || VBoxManage clonemedium $VDI_BASE data.vdi
+[ -f data.vdi ] || VBoxManage clonemedium $VDI_BASE data.vdi
 
